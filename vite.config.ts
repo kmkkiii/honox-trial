586ddd21872f4076dc3922328afe32d6d9297209
kmkkiii <rskkmkmk@gmail.com>
2024-02-10 14:48:07 +0900

    add devServer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import ssg from '@hono/vite-ssg'
 import mdx from '@mdx-js/rollup'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import devServer from '@hono/vite-dev-server'
 
 const entry = './app/server.ts'
 
@@ -11,6 +12,7 @@ export default defineConfig(() => {
   return {
     plugins: [
       honox(),
+      devServer({ entry }),
       ssg({ entry }),
       mdx({
         jsxImportSource: 'hono/jsx',
