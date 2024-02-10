28abd563bf86defbabcff54f6926dd22c1bcf02e
kmkkiii <rskkmkmk@gmail.com>
2024-02-10 14:40:59 +0900

    set mdx plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import pages from '@hono/vite-cloudflare-pages'
 import honox from 'honox/vite'
-import client from 'honox/vite/client'
 import { defineConfig } from 'vite'
+import ssg from '@hono/vite-ssg'
+import mdx from '@mdx-js/rollup'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
-export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      plugins: [client()]
-    }
-  } else {
-    return {
-      plugins: [honox(), pages()]
-    }
+const entry = './app/server.ts'
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      honox(),
+      ssg({ entry }),
+      mdx({
+        jsxImportSource: 'hono/jsx',
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      }),
+    ],
   }
 })
