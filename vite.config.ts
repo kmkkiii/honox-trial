2afa3fba87e425c58e7834eed8fbad7dd480e085
kmkkiii <rskkmkmk@gmail.com>
2024-02-11 09:42:54 +0900

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,17 +10,30 @@ import devServer from '@hono/vite-dev-server'
 
 const entry = './app/server.ts'
 
-export default defineConfig(() => {
-  return {
-    plugins: [
-      honox(),
-      devServer({ entry }),
-      ssg({ entry }),
-      mdx({
-        jsxImportSource: 'hono/jsx',
-        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter, remarkGfm],
-        rehypePlugins: [rehypeHighlight],
-      }),
-    ],
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      build: {
+        rollupOptions: {
+          input: ['/app/style.css'],
+          output: {
+            assetFileNames: 'static/assets/[name].[ext]',
+          },
+        },
+      },
+    }
+  } else {
+    return {
+      plugins: [
+        honox(),
+        // devServer({ entry }),
+        // ssg({ entry }),
+        mdx({
+          jsxImportSource: 'hono/jsx',
+          remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter, remarkGfm],
+          rehypePlugins: [rehypeHighlight],
+        }),
+      ],
+    }
   }
 })
